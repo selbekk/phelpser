93a32f239848b06f033cd9bb4d12410138a3de43
Kristofer Selbekk <selbeezy@gmail.com>
2017-03-13 18:47:33 +0100

    Make it a bit better

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Phelpser - crawl your website for 404s
 
-This is a super simple tool to crawl (see? a swimming pun!) your website (or somebody elses) for 404s.
+This is a super simple tool to crawl (see? a swimming pun!) your website (or
+somebody elses) for finding outdated URLs.
 
-It might not be the fastest tool, but it works just fine :)
+It might not be the fastest tool, but it works just fine for smaller websites.
 
 ## Usage
 
 ```bash
 yarn add --global phelpser
-phelpser http://www.your-website.com
+phelpser your-website.com
 ```
-
-It's still pretty stupid, so you have to add the protocol (http or https) and what not.
--- index.js
+++ index.js
@@ -6,10 +6,23 @@ const { get } = require('fetchutils');
 const jsdom = require('jsdom');
 require('colors');
 
+let urlToCrawl = process.argv[2];
+
+if (!urlToCrawl) {
+  console.error('You need to specify which website to crawl.'.red);
+  console.error('Usage: phelpser example.com');
+  return;
+}
+
+if (!urlToCrawl.startsWith('http')) {
+  urlToCrawl = `http://${urlToCrawl}`;
+}
+
+let [protocol, , domain] = urlToCrawl.split('/');
+domain = domain.replace('www.', '');
+
 const visitedPages = {};
 
-const urlToCrawl = process.argv[2];
-const [protocol, , domain] = urlToCrawl.split('/');
 const baseUrl = `${protocol}//${domain}`;
 
 const crawlLink = async (url) => {
@@ -25,12 +38,19 @@ const crawlLink = async (url) => {
             Array.from(window.document.querySelectorAll('a[href]'))
                 .map(el => el.getAttribute('href'))
                 .map(href => href.startsWith('/') ? baseUrl + href : href)
-                .filter(href => href.includes(baseUrl) && !visitedPages[href])
+                .filter(href => href.startsWith('http') && href.includes(domain) && !visitedPages[href])
                 .forEach(crawlLink);
         });
     } catch (e) {
-        if (e.status === 404) console.log(`${url} returned a 404`.bgRed)
-        else console.log(`${url} returned ${e.status}`)
+        if (e.status === 404) {
+          console.log(`${url} returned 404`.bgRed);
+        } else if (e.status) {
+          console.log(`${url} failed with a ${e.status} ${e.message}`.yellow);
+        } else if (e.code === 'ENOTFOUND') {
+          console.error(`Could not find the domain ${urlToCrawl}`.bgRed);
+        } else {
+          console.warn(`${url} threw an error:`.yellow, e);
+        }
         visitedPages[url] = { status: e.status };
     }
 }
